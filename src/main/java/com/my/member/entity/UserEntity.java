a12e0af2f00f2e089b5d83842459fce14a15de9e
kumanamu <serachstock1@gmail.com>
2025-08-18 17:00:03 +0900

    삭제 및 수정 업데이트

--- src/main/java/com/my/member/entity/UserEntity.java
+++ src/main/java/com/my/member/entity/UserEntity.java
@@ -12,10 +12,6 @@ import lombok.*;
 @Builder
 public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
